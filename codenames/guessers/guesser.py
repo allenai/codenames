d49d4fa7e13b52e4085ebf498c1955de04789e68
Pradeep Dasigi <pradeep.dasigi@gmail.com>
2018-08-08 15:14:25 -0700

    guesser, util, embedding handler and tests (#5)
    * guesser, util, embedding handler and tests
    
    * fixed type hint

---
+++ codenames/guessers/guesser.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from codenames.embedding_handler import EmbeddingHandler
+from codenames.util import UNREVEALED
+
+
+class Guesser:
+    """
+    Parameters
+    ----------
+    board : `List[str]`
+        List of all words on the board in the current game
+    embedding_file : `str`
+        Location of pickled embeddings
+    """
+    def __init__(self,
+                 board: List[str],
+                 embedding_handler: EmbeddingHandler) -> None:
+        self.board = board
+        self.embedding_handler = embedding_handler
+
+    def guess(self,
+              clue: str,
+              count: int,
+              game_state: List[int],
+              current_score: int) -> List[str]:
+        """
+        Parameters
+        ----------
+        clue : `str`
+        count : `int`
+            Max size of returned list
+        game_state : `List[int]`
+            List of same size as self.board, with each element showing the ids revealed so far (eg.
+            same team, opposite team, assasin, civilian etc.)
+        score : `int`
+            Current score
+        """
+        raise NotImplementedError
---
+++ tests/guessers/heuristic_guesser_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from codenames.guessers.heuristic_guesser import HeuristicGuesser
+from codenames.embedding_handler import EmbeddingHandler
+
+
+class TestHeuristicGuesser(TestCase):
+    def test_guess(self):
+        embedding_handler = EmbeddingHandler("tests/fixtures/sample_embedding.pkl")
+        sample_board = ["boy", "girl", "woman", "man"]
+        guesser = HeuristicGuesser(sample_board, embedding_handler)
+        sample_state = [0, -1, -1, -1]
+        guesses = guesser.guess("boy", 1, sample_state, 0)
+        assert guesses == ['girl', 'woman']  # count + 1
