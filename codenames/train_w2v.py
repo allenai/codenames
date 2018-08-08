808e2777322b6f99daa6e5095bd21940c8ef52c7
chandrab <chandrab@allenai.org>
2018-08-08 15:03:46 -0700

    Train word2vec

--- codenames/train_w2v.py
+++ codenames/train_w2v.py
@@ -3,23 +3,49 @@ import os
 
 from codenames.utils.file_utils import read_lines
 import spacy
+from gensim.models import Word2Vec
+
 
 def main(args):
     corpus_location = args.corpus_location
     save_dir = args.save_dir
+    workers = args.workers
 
-    tokenized_lowercased_file = os.path.join(os.path.dirname(corpus_location),
+    output_weights_file = os.path.join(save_dir, "weights.txt")
+    model_file = os.path.join(save_dir, "model.pkl")
+
+    tokenized_lowercased_filename = os.path.join(os.path.dirname(corpus_location),
                                              os.path.basename(corpus_location) + "-tokenized_lc.txt")
-    print("Writing to file: " + tokenized_lowercased_file)
+    print("Writing to file: " + tokenized_lowercased_filename)
 
-    if not os.path.exists(tokenized_lowercased_file):
+    all_sentences = []
+    if not os.path.exists(tokenized_lowercased_filename):
         nlp = spacy.load("en")
-        with open(tokenized_lowercased_file, "w") as tokenized_lc_file:
+        with open(tokenized_lowercased_filename, "w") as tokenized_lc_file:
             for line in read_lines(corpus_location):
                 sentence = line.split("\t")[1]
                 tokens = [w.text.lower() for w in nlp(sentence)]
-                tokenized_lc_file.write(' '.join(tokens))
+                sentence = ' '.join(tokens)
+                tokenized_lc_file.write(sentence)
+                tokenized_lc_file.write("\n")
+                all_sentences.append(sentence)
         tokenized_lc_file.close()
+    else:
+        all_sentences = read_lines(tokenized_lowercased_filename)
+
+    model = Word2Vec(
+        sentences=all_sentences,
+        size=300,
+        window=10,
+        workers=workers,
+        negative=10,
+        min_count=50,
+        sg=1,
+        iter=10
+    )
+
+    model.wv.save_word2vec_format(output_weights_file)
+    model.save(model_file)
 
 
 if __name__ == '__main__':
@@ -31,6 +57,9 @@ if __name__ == '__main__':
     parser.add_argument('--save_dir',
                         type=str,
                         help='location of training data')
+    parser.add_argument('--workers',
+                        type=int,
+                        help='number of workers')
 
     args = parser.parse_args()
     main(args)
