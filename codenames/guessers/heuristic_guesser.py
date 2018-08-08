8833a30df0c8743e7acef589265975d8dae0223a
pdasigi <pdasigi@cs.cmu.edu>
2018-08-08 14:56:54 -0700

    guesser, util, embedding handler and tests

---
+++ codenames/guessers/heuristic_guesser.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from overrides import overrides
+
+from codenames.guessers.guesser import Guesser
+import codenames.util as util
+
+
+class HeuristicGuesser(Guesser):
+    @overrides
+    def guess(self,
+              clue: str,
+              count: str,
+              game_state: List[int],
+              current_score: int) -> List[str]:
+        available_options = util.get_available_choices(self.board, game_state)
+        # Return 1 more than the count because the game rules allow it.
+        return self.embedding_handler.sort_options_by_similarity(clue,
+                                                                 available_options,
+                                                                 count + 1)
