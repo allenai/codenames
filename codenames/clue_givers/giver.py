5232e8bdf4c5d2d07b5115a196965113f0d8ce02
Nikita <qu@cs.washington.edu>
2018-08-08 18:08:46 -0700

    config file for some global speaker variables, basic giver class

---
+++ codenames/clue_givers/giver.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from codenames.embedding_handler import EmbeddingHandler
+#from codenames.util import UNREVEALED, GOOD, BAD, CIVILIAN, ASSASIN
+
+class Giver:
+    """
+    Parameters
+    ----------
+    board : `List[str]`
+        List of all words on the board in the current game
+    target_IDs: `List[str]`
+        List of all target words the Giver wants the Guesser to guess correctly
+    embedding_file : `str`
+        Location of pickled embeddings
+    """
+    def __init__(self,
+                 board: List[str],
+                 target_IDs: List[str],
+                 embedding_handler: EmbeddingHandler) -> None:
+        self.board = board
+        self.embedding_handler = embedding_handler
+   
+    def get_next_clue(self,
+                      game_state: List[int],
+                      current_score: int) -> List[Clue]:
+        """
+        Parameters
+        ----------
+        game_state : `List[int]`
+            List of same size as self.board, with each element showing the ids revealed so far (eg.
+            same team, opposite team, assasin, civilian etc.)
+        score : `int`
+            Current score
+        """
+        raise NotImplementedError
---
+++ config.py
@@ -0,0 +1,3 @@
+AGGRESSIVE_THRESHOLD = 4 #cluegiver becomes more aggressive with the ordering of the Best Clues list based on the score of the game. "Aggression" = more? fewer? words in Target_Words Group
+
+NUM_CLUES = 10 #number of clues to return in get_next_clue
