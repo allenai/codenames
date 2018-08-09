cfd2292ae31dfc0187647a4a265a632dabb09145
Nikita <qu@cs.washington.edu>
2018-08-09 16:54:25 -0700

    pseudo score/aggressiveness mechanic

--- codenames/clue_givers/giver.py
+++ codenames/clue_givers/giver.py
@@ -1,14 +1,7 @@
 from typing import List
-from collections import namedtuple
-from codenames.embedding_handler import EmbeddingHandler
-#from codenames.util import UNREVEALED, GOOD, BAD, CIVILIAN, ASSASIN
-#from codenames.gameplay.ai2_hack import Clue
-
 from codenames.utils.game_utils import Clue
 
 
-# from codenames.util import UNREVEALED, GOOD, BAD, CIVILIAN, ASSASIN
-#from codenames.gameplay.ai2_hack import Clue
 
 class Giver:
     """
@@ -16,19 +9,15 @@ class Giver:
     ----------
     board : `List[str]`
         List of all words on the board in the current game
-    target_IDs: `List[str]`
-        List of all target words the Giver wants the Guesser to guess correctly
-    embedding_file : `str`
-        Location of pickled embeddings
+    allIDs: `List[int]`
+        List of assignment codes for all words on the board
     """
 
     def __init__(self,
                  board: List[str],
                  allIDs: List[int]) -> None:
-                 #embedding_handler: EmbeddingHandler) -> None:
         self.board = board
         self.allIDs = allIDs
-        #self.embedding_handler = embedding_handler
 
     def get_next_clue(self,
                       game_state: List[int],
@@ -37,8 +26,7 @@ class Giver:
         Parameters
         ----------
         game_state : `List[int]`
-            List of same size as self.board, with each element showing the ids revealed so far (eg.
-            same team, opposite team, assasin, civilian etc.)
+            List of same size as self.board telling whether a word on board is already revealed
         score : `int`
             Current score
         """
