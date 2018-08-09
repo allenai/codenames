06d252a0c9b93bf894b1b3f2d5a221c6589dfedd
Katie Stasaki <katies@katies.local>
2018-08-08 19:40:11 -0700

    Model changes

---
+++ codenames/guessers/policy/guesser_policy.py
@@ -0,0 +1,13 @@
+import torch
+
+class Policy:
+    def __init__(self):
+        raise NotImplementedError
+
+    '''
+    Runs model forward to create a new policy for a given state.
+    
+    Inputs should be specified by child instantiations.`
+    '''
+    def forward(self)-> torch.Tensor:
+        raise NotImplementedError
--- codenames/guessers/policy/similarity_threshold.py
+++ codenames/guessers/policy/similarity_threshold.py
@@ -1,8 +1,12 @@
-from policy import Policy
 import torch as torch
 import torch.nn as nn
+from codenames.guessers.policy.guesser_policy import Policy
+
 
 class SimilarityThresholdPolicy(Policy, nn.Module):
+    '''
+    embed_size is the size of the word embeddings
+    '''
     def __init__(self, embed_size):
         super(Policy, self).__init__()
         self.embed_size = embed_size
@@ -10,14 +14,18 @@ class SimilarityThresholdPolicy(Policy, nn.Module):
         self.W = nn.Parameter(torch.Tensor(self.embed_size, self.embed_size), requires_grad = True)
 
         #Similarity threshold
-        self.threshold = nn.Parameter(torch.Tensor(1), requires_grad = True)
+        self.threshold = nn.Parameter(torch.Tensor([0.5]), requires_grad = True)
 
-    
+    '''
+        clue_vector has shape 1 x word_embed_size (represents the embedding of the clue word)
+        options_matrix has shape num_words_left_to_guess x word_embed_size (where num_words_left_to_guess is the 
+            remaining unguessed words on the game board)
+    '''
     def forward(self,
                 clue_vector: torch.Tensor,
                 options_matrix: torch.Tensor) -> torch.Tensor:
-
-        return (torch.matmul(torch.matmul(clue_vector, self.W), torch.t(options_matrix))) - self.threshold
+        m = nn.Sigmoid()
+        return m((torch.matmul(torch.matmul(clue_vector, self.W), torch.t(options_matrix)))) - self.threshold
 
 
 #Main method included only for testing purposes
