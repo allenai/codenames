9aa944cd20311000c2e2260523f46515d8434730
pdasigi <pdasigi@cs.cmu.edu>
2018-08-09 20:34:05 -0700

    refactor guesser and giver to use the same objects across games

--- codenames/guessers/heuristic_guesser.py
+++ codenames/guessers/heuristic_guesser.py
@@ -9,17 +9,17 @@ import codenames.utils.game_utils as util
 
 class HeuristicGuesser(Guesser):
 
-    def __init__(self, board: List[str], embedding_handler: EmbeddingHandler):
-        super().__init__(board)
+    def __init__(self, embedding_handler: EmbeddingHandler):
         self.embedding_handler = embedding_handler
 
     @overrides
     def guess(self,
+              board: List[str],
               clue: str,
               count: int,
               game_state: List[int],
               current_score: int) -> List[str]:
-        available_options = util.get_available_choices(self.board, game_state)
+        available_options = util.get_available_choices(board, game_state)
         # Return 1 more than the count because the game rules allow it.
         return self.embedding_handler.sort_options_by_similarity(clue,
                                                                  available_options,
